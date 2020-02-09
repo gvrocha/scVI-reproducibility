87d8bcfc26133764a3b7c7a9a8423ec7be928262
Guilherme Rocha <gvrocha@lilly.com>
2020-02-08 20:28:07 -0500

    Added script to download BRAIN-LARGE data set

---
+++ code/data_preparation/BRAIN-LARGE_download.sh
@@ -0,0 +1,5 @@
+outdir=/scVI/data
+mkdir -p $outdir/original/BRAIN-LARGE
+
+wget -v -O $outdir/original/BRAIN-LARGE/1M_neurons_filtered_gene_bc_matrices_h5.h5 http://cf.10xgenomics.com/samples/cell-exp/1.3.0/1M_neurons/1M_neurons_filtered_gene_bc_matrices_h5.h5
+
